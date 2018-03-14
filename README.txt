552fece72d6f877533ef154fe870f001344f348c
Stephen Fedele <stephen.m.fedele@wmich.edu>
2018-03-14 10:38:15 -0400

    Minor updates to 'main.c' and 'README.txt'.

--- README.txt
+++ README.txt
@@ -1,3 +1,12 @@
+Author: Stephen Fedele
+E-mail: stephen.m.fedele@wmich.edu
+
+The specification file can be found at 'Specification.png', and the file hierarchy layout can be found at 'LAYOUT.txt'.  
+
+This program and source code is currently (as of 2018:03:14) not to be re-distributed without the consent of the author.  
+
+Please contact the author at the given e-mail address with any questions that you may have.  
+
 Under Development Using:
 	g++ (Ubuntu 5.4.0-6ubuntu1~16.04.9) 5.4.0 20160609
 	flexc++ V2.05.00
--- main.cpp
+++ main.cpp
@@ -4,10 +4,12 @@ int main(){
 	//Open file as 'fp' and check for errors
 	//dup2 'stdout' as 'stdoutbkp'
 	//dup2 'fp' as 'stdout'
-	//Create a data structure for the program to run upon
-	//Call the parser wrapper
-	//
-	//dup2
-	
+	//Create a data structure for the program to run upon and check for errors
+	//Create a parser wrapper and check for errors
+	//Call the parser to create an execution chain and check for errors
+	//dup2 'stdoutbkp' to 'stdout'
+	//Call 'execute' upon the execution chain and the data structure and store the return value
+	//Delete the data structure, execution chain, and parser wrapper
+	//Print the return value
 	return 0;
 }
